ac93b14b0e77bf3b3ba765a8a2512fc23894d94e
Jhonatan Maia Rios <jhonatan.rios@the9level.com>
2020-08-31 08:30:31 -0300

    pdf reader inProgress

--- src/controller/DocumentPreviewController.js
+++ src/controller/DocumentPreviewController.js
@@ -1,3 +1,8 @@
+const pdfjsLib = require('pdfjs-dist')
+const path = require('path')
+
+pdfjsLib.GlobalWorkerOptions.workerSrc = path.resolve(__dirname, '../../dist/pdf.worker.bundle.js')
+
 export default class DocumentPreviewController{
     constructor(file){
         this._file = file
@@ -5,14 +10,15 @@ export default class DocumentPreviewController{
 
     getPreviewData(){
         return new Promise( (s, f) => {
+
+            let reader = new FileReader();
+
             switch(this._file.type){
 
                 case 'image/png':
                 case 'image/jpeg':
                 case 'image/jpg':
                 case 'image/gif':
-
-                    let reader = new FileReader()
 
                     reader.onload = e => {
                         s({
@@ -28,6 +34,22 @@ export default class DocumentPreviewController{
                 break;
 
                 case 'application/pdf':
+
+                    reader.onload = e => {
+
+                        pdfjsLib.getDocument(new Uint8Array(reader.result)).then(pdf => {
+
+                            console.log('pdf', pdf)
+
+                        }).catch( err => {
+                            f(err)
+                        })
+
+                    }
+
+                    reader.readAsArrayBuffer(this._file)
+
+
                 break
                 default:
                     f()
